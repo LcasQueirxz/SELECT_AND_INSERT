b2dee20872ce9d769fbd504bfa354ddba7eee08d
LcasQueirxz <161960484+LcasQueirxz@users.noreply.github.com>
2024-11-04 23:53:27 -0300

    new update

--- Dump20241101 (2)/video_locadora_categoria.sql
+++ Dump20241101 (2)/video_locadora_categoria.sql
@@ -35,7 +35,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'Ação'),(2,'Romance'),(3,'Aventura'),(4,'Ficção'),(5,'Drama'),(6,'Terror'),(7,'Desenho'),(8,'Policial'),(9,'Comédia');
+INSERT INTO `categoria` VALUES (1,'Ação'),(2,'Romance'),(3,'Aventura'),(4,'Ficção'),(5,'Drama'),(6,'Terror'),(7,'ANIMAÇÃO'),(8,'Policial'),(9,'Comédia');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-01 20:56:26
+-- Dump completed on 2024-11-04 23:52:32
--- Dump20241101 (2)/video_locadora_filme.sql
+++ Dump20241101 (2)/video_locadora_filme.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.38, for macos14 (x86_64)
 --
--- Host: 127.0.0.1    Database: videolocadora
+-- Host: 127.0.0.1    Database: video_locadora
 -- ------------------------------------------------------
 -- Server version	8.0.39
 
@@ -32,7 +32,7 @@ CREATE TABLE `filme` (
   PRIMARY KEY (`cod_filme`),
   KEY `fk_categoria_filme` (`cod_categoria`),
   CONSTRAINT `fk_categoria_filme` FOREIGN KEY (`cod_categoria`) REFERENCES `categoria` (`cod_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `filme` (
 
 LOCK TABLES `filme` WRITE;
 /*!40000 ALTER TABLE `filme` DISABLE KEYS */;
-INSERT INTO `filme` VALUES (1,'300',1,'Richard Donner',3.5,'n'),(2,'Máquina Mortífera',1,'Richard Donner',3.6,'n'),(3,'A Mexicana',2,'Burr Steers',2,'s'),(4,'A Verdade Nua e Crua',2,'Robert Luketic',4,'n'),(5,'A vida é bela',2,'Roberto Benigni',3.5,'s'),(6,'Austrália',3,'Baz Luhrmann',4,'s'),(7,'Ultimato Bourn',3,'Paul Greengrass',3.5,'n'),(8,'Constantine',4,'Francis Lawrence',2.5,'s'),(9,'Os Irmãos Grimm',4,'Terry Gilliam',3.5,'n'),(10,'Os Doze Macacos',4,'Terry Gilliam',2.5,'n'),(11,'Amadeus',5,'Milos Forman',10,'n'),(12,'As Torres Gêmeas',5,'Oliver Stone',2.5,'s'),(13,'Platoon',1,'Oliver Stone',5.5,'s'),(14,'O Advogado do Diabo',6,'Taylor Hackford',1.5,'s'),(15,'Beowulf',7,'Robert Zemeckis',1,'n'),(16,'Bolt o super cão',7,'Byron Howard',1.5,'s'),(17,'Apertem o cinto o piloto sumiu',9,'Jim Abrahams',3.6,'s'),(18,'Doze é demais',9,'Shawn Levy',9.2,'s'),(19,'Uma noite no museu',9,'Shawn Levy',2.5,'n');
+INSERT INTO `filme` VALUES (1,'300',1,'Richard Donner',3.5,'n'),(2,'Máquina Mortífera',1,'Richard Donner',3.6,'n'),(3,'A Mexicana',2,'Burr Steers',2,'s'),(4,'A Verdade Nua e Crua',2,'Robert Luketic',4,'n'),(5,'A vida é bela',2,'Roberto Benigni',3.5,'s'),(6,'Austrália',3,'Baz Luhrmann',4,'s'),(7,'Ultimato Bourn',3,'Paul Greengrass',3.5,'n'),(8,'Constantine',4,'Francis Lawrence',2.5,'s'),(9,'Os Irmãos Grimm',4,'Terry Gilliam',3.5,'s'),(10,'Os Doze Macacos',4,'Terry Gilliam',2.5,'s'),(11,'Amadeus',5,'Milos Forman',10,'n'),(12,'As Torres Gêmeas',5,'Oliver Stone',2.5,'s'),(13,'Platoon',1,'Oliver Stone',5.5,'s'),(14,'O Advogado do Diabo',6,'Taylor Hackford',1.5,'s'),(15,'Beowulf',7,'Robert Zemeckis',1,'n'),(16,'Bolt o super cão',7,'Byron Howard',1.5,'s'),(17,'Apertem o cinto o piloto sumiu',9,'Jim Abrahams',3.6,'s'),(18,'Doze é demais',9,'Shawn Levy',9.2,'s'),(19,'Uma noite no museu',9,'Shawn Levy',2.5,'n'),(20,'Missão Impossível: 3',1,'J.J. Abrams',7,'n'),(21,'Missão Impossível: 3',1,'J.J. Abrams',7,'n'),(22,'Missão Impossível: 3',1,'J.J. Abrams',7,'n'),(23,'Missão Impossível: 3',1,'J.J. Abrams',7,'n'),(24,'Missão Impossível: 3',1,'J.J. Abrams',7,'n');
 /*!40000 ALTER TABLE `filme` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-01 20:56:26
+-- Dump completed on 2024-11-04 23:52:32
